4fcb1ec9420534dec93f60290607627909f0fb94
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-05-29 13:26:41 +0000

    Initial Commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,63 @@
+class BooksController < ApplicationController
+  
+  before_action :correct_user, only: [:edit, :update]
+
+  def show
+    @book = Book.find(params[:id])
+    @user = @book.user
+    @booknew = Book.new
+    @book_comment = BookComment.new
+  end
+  
+
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path(@book), notice: "You have created book successfully."
+    else
+      @books = Book.all
+      render 'index'
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book), notice: "You have updated book successfully."
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body, :rate)
+  end
+  
+  
+  def correct_user
+    book = Book.find(params[:id])
+    if current_user.id != book.user_id
+      redirect_to books_path
+    end
+  end
+  
+end
+
